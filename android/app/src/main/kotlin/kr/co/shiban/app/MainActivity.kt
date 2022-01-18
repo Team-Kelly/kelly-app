6455bbc4cb22eef816540e10d4e47c6f376203ab
DokySp <uhug@naver.com>
2022-01-18 16:15:14 +0900

    보일러플레이트 작성

---
+++ android/app/src/main/kotlin/kr/co/shiban/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package kr.co.shiban.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+
+
+import 'package:app/view/splash.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const MyApp());
+}
+
+
+class MyApp extends StatefulWidget {
+  const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const SplashView(),
+      routes: {
+        "/": (_) => const SplashView(),
+      },
+    );
+  }
+}
